561c84545bb722fdafe3e6d35b1762d57a7bdc3e
SkyLightQP <combbm@naver.com>
2019-09-29 21:40:13 +0900

    feat: apply Google Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import App from './App';
 import 'bulma/css/bulma.css';
 import 'bulma-timeline/dist/css/bulma-timeline.min.css';
 
+ReactGA.initialize('UA-69668170-5');
+
+function logPageView() {
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+}
+
 const router = (
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={App}/>
+            <Route exact path="/" component={App} onUpdate={logPageView}/>
         </Switch>
     </BrowserRouter>
 );
