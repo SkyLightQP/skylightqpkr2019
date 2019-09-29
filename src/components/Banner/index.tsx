995b260504debb8fc541eedad099f511c4b658ef
SkyLightQP <combbm@naver.com>
2019-09-29 15:47:50 +0900

    feat: apply webfont
    - refactor: apply eslint

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Banner: React.FC = () => {
     return (
-        <section className="hero is-dark">
-            <div className="hero-body">
-                <div className="container">
-                    <h1 className="title">
+        <section className='hero is-dark is-medium'>
+            <div className='hero-body'>
+                <div className='container'>
+                    <h1 className='title'>
                         Hero title
                     </h1>
-                    <h2 className="subtitle">
+                    <h2 className='subtitle'>
                         Hero subtitle
                     </h2>
                 </div>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TitleStyle = styled.p`
+  font-size: 20px;
+  font-weight: 500;
+`;
+
 
 const Navbar: React.FC = () => {
     return (
-        <nav className='navbar' role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <a className="navbar-item" href="https://bulma.io">
-                    <img
-                      src="https://bulma.io/images/bulma-logo.png"
-                      alt="Bulma: Free, open source, & modern CSS framework based on Flexbox"
-                      width="112"
-                      height="28"
-                    />
+        <nav className='navbar container' role='navigation' aria-label='main navigation'>
+            <div className='navbar-brand'>
+                <a className='navbar-item' href='https://bulma.io'>
+                    <TitleStyle>하늘빛</TitleStyle>
                 </a>
             </div>
         </nav>
