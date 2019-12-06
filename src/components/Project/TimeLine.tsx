d29a3cc66726d0e2b84101fc90d7e0306157d6c3
SkyLightQP <combbm@naver.com>
2019-12-06 21:26:10 +0900

    feat: add new project in timeline
    - feat add TimeLineItem component

--- src/components/Project/TimeLine.tsx
+++ src/components/Project/TimeLine.tsx
@@ -1,110 +1,33 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const SmallTextStyle = styled.p`
-  font-size: 16px;
-`;
+import TimeLineItem from './TimeLineItem';
 
 const TimeLine: React.FC = () => {
     return (
         <div className='timeline'>
-            <div className='timeline-item'>
-                <div className='timeline-marker'/>
-                <div className='timeline-content'>
-                    <p className='heading'>2017 03</p>
-                    <SmallTextStyle>
-                        온라인 끝말잇기 웹게임 끄투닷컴 운영
-                    </SmallTextStyle>
-                </div>
+            <div className='timeline-header'>
+                <span className='tag is-dark'>2017</span>
             </div>
 
-            <div className='timeline-item'>
-                <div className='timeline-marker'/>
-                <div className='timeline-content'>
-                    <p className='heading'>2017 09</p>
-                    <SmallTextStyle>
-                        온도, 습도, 미세먼지 값을 측정하여 웹으로 보여주는 AirQualityTool
-                    </SmallTextStyle>
-                </div>
-            </div>
+            <TimeLineItem date="2017 03">온라인 끝말잇기 웹게임 끄투닷컴 운영</TimeLineItem>
+            <TimeLineItem date="2017 09">온도, 습도, 미세먼지 값을 측정하여 웹으로 보여주는 AirQualityTool</TimeLineItem>
 
             <div className='timeline-header'>
                 <span className='tag is-dark'>2018</span>
             </div>
 
-            <div className='timeline-item'>
-                <div className='timeline-marker'/>
-                <div className='timeline-content'>
-                    <p className='heading'>2018 11</p>
-                    <SmallTextStyle>
-                        각종 기능이 추가된 디스코드 봇. 달콤한 봇
-                    </SmallTextStyle>
-                </div>
-            </div>
-
-            <div className='timeline-item'>
-                <div className='timeline-marker'/>
-                <div className='timeline-content'>
-                    <p className='heading'>2018 12</p>
-                    <SmallTextStyle>
-                        2019년 카운트다운 사이트. 안녕! 2019!
-                    </SmallTextStyle>
-                </div>
-            </div>
+            <TimeLineItem date="2018 11">각종 기능이 추가된 디스코드 봇. 달콤한 봇</TimeLineItem>
+            <TimeLineItem date="2018 12">2019년 카운트다운 사이트. 안녕! 2019!</TimeLineItem>
 
             <div className='timeline-header'>
                 <span className='tag is-dark'>2019</span>
             </div>
 
-            <div className='timeline-item'>
-                <div className='timeline-marker'/>
-                <div className='timeline-content'>
-                    <p className='heading'>2019 01</p>
-                    <SmallTextStyle>
-                        익명 질문 사이트. Ask!
-                    </SmallTextStyle>
-                </div>
-            </div>
-
-            <div className='timeline-item'>
-                <div className='timeline-marker'/>
-                <div className='timeline-content'>
-                    <p className='heading'>2019 02</p>
-                    <SmallTextStyle>
-                        끄투 TypeScript + React 리메이크 프로젝트. 쿵따(Kungtta)
-                    </SmallTextStyle>
-                </div>
-            </div>
-
-            <div className='timeline-item'>
-                <div className='timeline-marker'/>
-                <div className='timeline-content'>
-                    <p className='heading'>2019 04</p>
-                    <SmallTextStyle>
-                        교내 시간표, 급식 알리미 디스코드 봇. KrónosBot
-                    </SmallTextStyle>
-                </div>
-            </div>
-
-            <div className='timeline-item'>
-                <div className='timeline-marker'/>
-                <div className='timeline-content'>
-                    <p className='heading'>2019 06</p>
-                    <SmallTextStyle>
-                        교내 문제풀이 및 커뮤니티 사이트. 수프트
-                    </SmallTextStyle>
-                </div>
-            </div>
-
-            <div className='timeline-item'>
-                <div className='timeline-marker'/>
-                <div className='timeline-content'>
-                    <p className='heading'>2019 11</p>
-                    <SmallTextStyle>
-                        학교 진로 프로젝트: 저작물 도용 신고 서비스. PASC
-                    </SmallTextStyle>
-                </div>
-            </div>
+            <TimeLineItem date="2019 01">익명 질문 사이트. Ask!</TimeLineItem>
+            <TimeLineItem date="2019 02">끄투 TypeScript + React 리메이크 프로젝트. 쿵따(Kungtta)</TimeLineItem>
+            <TimeLineItem date="2019 04">교내 시간표, 급식 알리미 디스코드 봇. KrónosBot</TimeLineItem>
+            <TimeLineItem date="2019 06">교내 문제풀이 및 커뮤니티 사이트. 수프트</TimeLineItem>
+            <TimeLineItem date="2019 11">학교 진로 프로젝트: 저작물 도용 신고 서비스. PASC</TimeLineItem>
+            <TimeLineItem date="2019 12">제2회 KB국민은행 소프트웨어 경진 대회. DOP 프론트엔드(frontend) 참여</TimeLineItem>
 
             <div className='timeline-header'>
                 <span className='tag is-dark'>NOW</span>
